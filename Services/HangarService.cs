15ea16d8bbebd80461bcbdfd7336cc4cf45356c7
jorgedan88 <129257560+jorgedan88@users.noreply.github.com>
2023-06-12 02:00:54 -0300

    Se inicia el proyecto agregando la carpeta services con HangarService y su interface, no se toca el HangarController aun

---
+++ Services/HangarService.cs
@@ -0,0 +1,32 @@
+using ProyectoPrimerParcial.Models;
+
+
+namespace ProyectoPrimerParcial2.Services;
+
+public class HangarService : IHangarService
+{
+    public void Create(Hangar obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Hangar obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Hangar> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Hangar GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Hangar obj)
+    {
+        throw new NotImplementedException();
+    }
+}
